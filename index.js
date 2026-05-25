3ff02a300634e1ef50a881c5cdc309272b4460dc
codingnekochan <ibrahimoyiza198@gmail.com>
2026-05-25 12:55:35 +0100

    add build config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("node:path");
 const boardRouter = require("./routes/board");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const LOCAL_HOST = "127.0.0.1";
 
 app.set("views", path.join(__dirname, "views"));
